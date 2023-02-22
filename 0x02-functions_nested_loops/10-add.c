44b9be90c17e299e75dc5e57d55da37f6c1510ba
Opeoluwa Okonkwo <opeoluwa.okonkwo@yahoo.com>
2023-02-21 19:08:14 -0800

    202302217

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
+
