1544b73e68f752cf07a541badbc0089431d20cdd
Opeoluwa Okonkwo <opeoluwa.okonkwo@yahoo.com>
2023-02-23 17:17:00 -0800

    2023022316:16

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: character to be checked if digit or not
+ *
+ * Return: 1 if digit, 0 if not digit
+ */
+
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result (multiplication of 2 integers)
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
