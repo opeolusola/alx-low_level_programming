f3ae624c5235b6a9cca96f7769e8c7b74acaf5a9
Opeoluwa Okonkwo <opeoluwa.okonkwo@yahoo.com>
2023-02-24 06:14:01 -0800

    202302247:13

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,3 +13,4 @@ void print_number(int n);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+void positive_or_negative(int i)
